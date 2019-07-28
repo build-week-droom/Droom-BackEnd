e57ca37d1761d5ac19b7d13b199b267dd1dc5ad4
Noble Obioma <nobioma1@gmail.com>
2019-07-28 18:37:54 +0100

    Add jobs route to app route and validateToken middleware

--- api/appRouter.js
+++ api/appRouter.js
@@ -2,8 +2,11 @@ const appRouter = require('express').Router();
 
 const authRouter = require('./auth/authRouter');
 const companyRouter = require('./company/companyRouter');
+const { validateToken } = require('./auth/authMiddleware');
+const jobsRouter = require('./jobs/jobsRouter');
 
 appRouter.use('/auth', authRouter);
 appRouter.use('/company', companyRouter);
+appRouter.use('/jobs', validateToken, jobsRouter);
 
 module.exports = appRouter;
