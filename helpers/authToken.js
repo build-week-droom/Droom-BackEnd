0d646bfcafc780b105daf414526ecdc96819c219
Noble Obioma <nobioma1@gmail.com>
2019-07-28 15:11:45 +0100

    Create functions to generate and verify jwt tokens

---
+++ helpers/authToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET
+  || 'Hey, You are not supposed to see this. Im telling yo mama';
+
+module.exports = {
+  generateToken: ({ id, email, isCompany }) => {
+    const payload = { id, email, isCompany };
+    const options = { expiresIn: '24h' };
+
+    return jwt.sign(payload, secret, options);
+  },
+  verifyToken: token => jwt.verify(token, secret),
+};
