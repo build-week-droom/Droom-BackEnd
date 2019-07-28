f3c421e8d2b8750c57cfb77f55a7f4a09477f065
Noble Obioma <nobioma1@gmail.com>
2019-07-28 15:21:16 +0100

    Create regster and login controllers

---
+++ api/auth/authController.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcryptjs');
+
+const Users = require('./authModel');
+const { generateToken } = require('../../helpers/authToken');
+
+async function register(req, res) {
+  try {
+    const [user] = await Users.add(req.body);
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).json({ error: 'Error Creating User' });
+  }
+}
+
+async function login(req, res) {
+  const { user, body } = req;
+  if (bcrypt.compareSync(body.password, user.password)) {
+    const token = generateToken(user);
+    return res.status(200).json({
+      message: `Welcome, ${user.name}!`,
+      token,
+    });
+  }
+  return res.status(401).json({ error: 'Incorrect User Credentials' });
+}
+
+module.exports = { register, login };
