eac80f643f59753d36b31a3a542a261e09189404
Noble Obioma <nobioma1@gmail.com>
2019-07-28 19:28:11 +0100

    Create controller to update and get a jobseeker's profile

---
+++ api/jobSeekers/jobSeekerController.js
@@ -0,0 +1,17 @@
+const JobSeeker = require('./jobSeekerModel');
+
+function getProfile(req, res) {
+  return res.status(200).json(req.profile);
+}
+
+async function updateProfile(req, res) {
+  const { body, authUser } = req;
+  try {
+    const profileChanges = await JobSeeker.updateProfile(authUser.id, body);
+    return res.status(200).json(profileChanges);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = { getProfile, updateProfile };
