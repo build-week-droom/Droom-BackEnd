16798eb8f830ae9413cbde6948c1b82d4c281720
Noble Obioma <nobioma1@gmail.com>
2019-07-28 19:27:25 +0100

    Require fields on company schema

--- api/company/companySchema.js
+++ api/company/companySchema.js
@@ -2,6 +2,6 @@ const Joi = require('@hapi/joi');
 
 module.exports = Joi.object().keys({
   about: Joi.string().required(),
-  location: Joi.string().allow(''),
+  location: Joi.string().required(),
   profileImg: Joi.string().allow(''),
 });
