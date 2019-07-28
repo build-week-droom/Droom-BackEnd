e08adea9fec6772686e9187531cb033091f34c9b
Noble Obioma <nobioma1@gmail.com>
2019-07-28 19:26:46 +0100

    Add validate company role on put route

--- api/company/companyRouter.js
+++ api/company/companyRouter.js
@@ -1,12 +1,12 @@
 const companyRouter = require('express').Router();
 
 const { getCompanyProfile, updateCompanyProfile } = require('./companyController');
-const { checkCompanyExists } = require('./companyMiddleware');
+const { checkCompanyExists, validateCompanyRole } = require('./companyMiddleware');
 const { validateToken } = require('../auth/authMiddleware');
 const validate = require('../../helpers/validate');
 const profileSchema = require('./companySchema');
 
 companyRouter.get('/:id', checkCompanyExists, getCompanyProfile);
-companyRouter.put('/', validateToken, validate(profileSchema), updateCompanyProfile);
+companyRouter.put('/', validateToken, validateCompanyRole, validate(profileSchema), updateCompanyProfile);
 
 module.exports = companyRouter;
