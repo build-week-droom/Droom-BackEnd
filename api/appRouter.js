42430721fdb62a73f02b75e2b8d6d2479e4ea3b6
Noble Obioma <nobioma1@gmail.com>
2019-07-28 18:16:39 +0100

    add the company router to the app router

--- api/appRouter.js
+++ api/appRouter.js
@@ -1,7 +1,9 @@
 const appRouter = require('express').Router();
 
 const authRouter = require('./auth/authRouter');
+const companyRouter = require('./company/companyRouter');
 
 appRouter.use('/auth', authRouter);
+appRouter.use('/company', companyRouter);
 
 module.exports = appRouter;
