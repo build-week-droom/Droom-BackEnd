51d8160a07fb49f65791d8562bf7561d4bd51915
Noble Obioma <nobioma1@gmail.com>
2019-07-28 18:14:45 +0100

    Create schema to validate profile input

---
+++ api/company/companySchema.js
@@ -0,0 +1,7 @@
+const Joi = require('@hapi/joi');
+
+module.exports = Joi.object().keys({
+  about: Joi.string().required(),
+  location: Joi.string().allow(''),
+  profileImg: Joi.string().allow(''),
+});
