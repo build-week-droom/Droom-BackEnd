041376acaeb5c46c7addfb946cbb91445efe26e8
Noble Obioma <nobioma1@gmail.com>
2019-07-28 19:28:54 +0100

    Add job seeker route to app router

--- api/appRouter.js
+++ api/appRouter.js
@@ -4,9 +4,11 @@ const authRouter = require('./auth/authRouter');
 const companyRouter = require('./company/companyRouter');
 const { validateToken } = require('./auth/authMiddleware');
 const jobsRouter = require('./jobs/jobsRouter');
+const jobSeekersRouter = require('./jobSeekers/jobSeekerRouter');
 
 appRouter.use('/auth', authRouter);
 appRouter.use('/company', companyRouter);
 appRouter.use('/jobs', validateToken, jobsRouter);
+appRouter.use('/seekers', validateToken, jobSeekersRouter);
 
 module.exports = appRouter;
