16a85e10ff1b50194332c7db4afe36cb06b3dccc
Noble Obioma <nobioma1@gmail.com>
2019-07-29 05:34:34 +0100

    Add checkIfJobByUser middleware on routes put and delete

--- api/jobs/jobsRouter.js
+++ api/jobs/jobsRouter.js
@@ -3,13 +3,26 @@ const jobsRouter = require('express').Router();
 const jobs = require('./jobsController');
 const validate = require('../../helpers/validate');
 const jobSchema = require('./jobSchema');
-const { checkJobExists } = require('./jobsMiddleware');
+const { checkJobExists, checkIfJobByUser } = require('./jobsMiddleware');
 const { validateCompanyRole } = require('../company/companyMiddleware');
 
 jobsRouter.get('/', jobs.getAllJobs);
 jobsRouter.get('/:id', checkJobExists, jobs.getJob);
 jobsRouter.post('/', validateCompanyRole, validate(jobSchema), jobs.createJob);
-jobsRouter.put('/:id', validateCompanyRole, checkJobExists, validate(jobSchema), jobs.updateJob);
-jobsRouter.delete('/:id', validateCompanyRole, checkJobExists, jobs.deleteJob);
+jobsRouter.put(
+  '/:id',
+  validateCompanyRole,
+  checkJobExists,
+  checkIfJobByUser,
+  validate(jobSchema),
+  jobs.updateJob,
+);
+jobsRouter.delete(
+  '/:id',
+  validateCompanyRole,
+  checkJobExists,
+  checkIfJobByUser,
+  jobs.deleteJob,
+);
 
 module.exports = jobsRouter;
