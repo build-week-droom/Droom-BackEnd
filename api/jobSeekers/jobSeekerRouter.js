8d664c66d702b61bbd9d9e315f02163071700823
Noble Obioma <nobioma1@gmail.com>
2019-07-29 05:06:49 +0100

    Create route to get all job seekers

--- api/jobSeekers/jobSeekerRouter.js
+++ api/jobSeekers/jobSeekerRouter.js
@@ -1,11 +1,12 @@
 const jobSeekersRouter = require('express').Router();
 
-const { getProfile, updateProfile } = require('./jobSeekerController');
+const { getProfile, updateProfile, getAllProfiles } = require('./jobSeekerController');
 const { checkUserExists, validateUserRole } = require('./jobSeekerMiddleware');
 const { validateToken } = require('../auth/authMiddleware');
 const validate = require('../../helpers/validate');
 const profileSchema = require('./jobSeekerSchema');
 
+jobSeekersRouter.get('/', validateToken, getAllProfiles);
 jobSeekersRouter.get('/:id', checkUserExists, getProfile);
 jobSeekersRouter.put('/', validateToken, validateUserRole, validate(profileSchema), updateProfile);
 
