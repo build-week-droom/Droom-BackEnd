20cc959249618ac1b8fe0c49dbf14d8da0b57304
Noble Obioma <nobioma1@gmail.com>
2019-07-29 05:33:02 +0100

    Create middleware to validate if operation on item was created by current logged in user

--- api/jobs/jobsMiddleware.js
+++ api/jobs/jobsMiddleware.js
@@ -14,4 +14,20 @@ const checkJobExists = async (req, res, next) => {
   }
 };
 
-module.exports = { checkJobExists };
+const checkIfJobByUser = async (req, res, next) => {
+  const {
+    job: { id },
+    authUser,
+  } = req;
+  try {
+    const job = await Jobs.get(id, authUser.id);
+    if (job) {
+      return next();
+    }
+    throw new Error('You do not have permission for operation');
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { checkJobExists, checkIfJobByUser };
