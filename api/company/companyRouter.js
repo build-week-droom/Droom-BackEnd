d71d2a76fb37589e3fabe4f3fdfd39e632ef7ad7
Noble Obioma <nobioma1@gmail.com>
2019-07-30 13:12:33 +0100

    Create Route to get all company profile

--- api/company/companyRouter.js
+++ api/company/companyRouter.js
@@ -1,11 +1,12 @@
 const companyRouter = require('express').Router();
 
-const { getCompanyProfile, updateCompanyProfile } = require('./companyController');
+const { getAllCompanies, getCompanyProfile, updateCompanyProfile } = require('./companyController');
 const { checkCompanyExists, validateCompanyRole } = require('./companyMiddleware');
 const { validateToken } = require('../auth/authMiddleware');
 const validate = require('../../helpers/validate');
 const profileSchema = require('./companySchema');
 
+companyRouter.get('/', getAllCompanies);
 companyRouter.get('/:id', checkCompanyExists, getCompanyProfile);
 companyRouter.put('/', validateToken, validateCompanyRole, validate(profileSchema), updateCompanyProfile);
 
