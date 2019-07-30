3c52dd726302f065370aa5cee3e95d8e4fb2e306
Noble Obioma <nobioma1@gmail.com>
2019-07-30 13:15:12 +0100

    Create test seeds

---
+++ db/seeds/test/001-users.js
@@ -0,0 +1,30 @@
+const generatePassword = require('../../../helpers/generatePassword');
+
+exports.seed = knex => knex('users')
+  .del()
+  .then(() => knex('users').insert([
+    {
+      name: 'test company1',
+      email: 'testcompany1@test.com',
+      password: generatePassword('test'),
+      isCompany: true,
+    },
+    {
+      name: 'test company2',
+      email: 'testcompany2@test.com',
+      password: generatePassword('test'),
+      isCompany: true,
+    },
+    {
+      name: 'test jobseeker1',
+      email: 'testjobseeker1@test.com',
+      password: generatePassword('test'),
+      isCompany: false,
+    },
+    {
+      name: 'test jobseeker2',
+      email: 'testjobseeker2@test.com',
+      password: generatePassword('test'),
+      isCompany: false,
+    },
+  ]));
