dafc99d8ca2187c6d96bc431b0dd53f09d74884f
Noble Obioma <nobioma1@gmail.com>
2019-07-31 13:16:11 +0100

    Create routing for matches

--- api/appRouter.js
+++ api/appRouter.js
@@ -1,14 +1,16 @@
 const appRouter = require('express').Router();
 
+const { validateToken } = require('./auth/authMiddleware');
 const authRouter = require('./auth/authRouter');
 const companyRouter = require('./company/companyRouter');
-const { validateToken } = require('./auth/authMiddleware');
 const jobsRouter = require('./jobs/jobsRouter');
 const jobSeekersRouter = require('./jobSeekers/jobSeekerRouter');
+const matchesRouter = require('./matches/matchesRouter');
 
 appRouter.use('/auth', authRouter);
 appRouter.use('/company', companyRouter);
 appRouter.use('/jobs', validateToken, jobsRouter);
 appRouter.use('/seekers', validateToken, jobSeekersRouter);
+appRouter.use('/matches', validateToken, matchesRouter);
 
 module.exports = appRouter;
